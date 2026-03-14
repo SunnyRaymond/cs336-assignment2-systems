aae4c87478db20a0f7c48cf434aeb0cea964dbaf
Raymond Guo <yguo017@e.ntu.edu.sg>
2026-03-14 22:04:06 +0800

    update nsys_profile.sh

--- nsys_profile.sh
+++ nsys_profile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=nsys_profile
 #SBATCH --partition=fatq
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:4
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=64G
 #SBATCH --time=2-00:00:00
