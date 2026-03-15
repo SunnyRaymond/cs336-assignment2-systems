f7a4707befa26913d040aaa07b84c9ffbe5e21e2
Raymond Guo <yguo017@e.ntu.edu.sg>
2026-03-15 12:40:29 +0800

    update minimal_ddp_flat_benchmark.sh

--- minimal_ddp_flat_benchmark.sh
+++ minimal_ddp_flat_benchmark.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=minimal_ddp_flat
-#SBATCH --partition=fatq
 #SBATCH --gres=gpu:2
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=64G
-#SBATCH --time=0-00:30:00
+#SBATCH --time=1-00:30:00
 #SBATCH --output=log/%x_%j.log
 #SBATCH --error=log/%x_%j.log
 
