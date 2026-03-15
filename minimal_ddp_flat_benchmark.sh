6e89917427887fd2407a28a0eaaf444ef2c8bb31
Raymond Guo <yguo017@e.ntu.edu.sg>
2026-03-15 12:38:59 +0800

    update 11 files

--- minimal_ddp_flat_benchmark.sh
+++ minimal_ddp_flat_benchmark.sh
@@ -17,7 +17,12 @@ module load cuda12.6/toolkit/12.6
 PROJECT_ROOT="${SLURM_SUBMIT_DIR:-$(pwd)}"
 cd "$PROJECT_ROOT"
 
+echo "change uv cache dir to /var/scratch/dpp2567/.uv_cache"
 source /var/scratch/dpp2567/miniconda3/etc/profile.d/conda.sh
+export UV_CACHE_DIR=/var/scratch/dpp2567/.uv_cache
+export XDG_CACHE_HOME=/var/scratch/dpp2567/.cache
+export UV_PROJECT_ENVIRONMENT=/var/scratch/dpp2567/.venvs/cs336-assignment2-systems
+
 conda activate base
 
 mkdir -p profiles/ddp
